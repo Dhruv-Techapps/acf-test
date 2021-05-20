36507471421bf13e8f6eea1b7948dfcbaa9568d9
Dharmesh <dharmesh.hemaram@gmail.com>
2021-05-20 18:54:55 +0530

    added more event listener

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,7 +2,7 @@ window.onload = function () {
  $("#click-events-test").bind("mouseover mousedown mouseup click dblclick blur focus submit", function (event) {
   console.log(event.type, event.currentTarget);
  });
- $("#form-events-test").bind("blur focus submit keypress keydown keyup", function (event) {
-  console.log(event.type, event.key, event.keyCode, event);
+ $("#form-events-test").bind("blur focus submit keypress keydown keyup input change", function (event) {
+  console.log(event.type, event.key, event.keyCode);
  });
 }
